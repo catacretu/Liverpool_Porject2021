53ee4b1ad0e532e2391c0fb678e574d431ca95c8
catacretu <48598290+catacretu@users.noreply.github.com>
2019-05-20 18:54:28 +0300

    Liverpool

--- app/src/main/java/com/example/liverpool/MainActivity.java
+++ app/src/main/java/com/example/liverpool/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         findViewById(R.id.history).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
